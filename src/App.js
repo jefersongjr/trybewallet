a28b8e306e2755bd84599dc51a44df57aed985c0
jefersongjr <jeferson_gjr@hotmail.com>
2022-06-22 15:59:53 -0300

    Criando formulário de Login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,22 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Ola moundp</div>;
+    return (
+      <section className="login-box">
+        <label name="login-name">
+          Username:
+          <input name="login-name" type="email" data-testid="email-input" />
+        </label>
+
+        <label name="password-name">
+          Password:
+          <input name="password-name" type="password" data-testid="password-input" />
+        </label>
+        <button type="button">
+          Entrar
+        </button>
+      </section>
+    );
   }
 }
 
