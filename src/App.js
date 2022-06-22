90851370ebbe558cb4560f518cdac51560c3a271
jefersongjr <jeferson_gjr@hotmail.com>
2022-06-22 15:27:51 -0300

    Criando as rotas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
-function App() {
-  return <div>Hello, TrybeWallet! Iniciando</div>;
+class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <Route exact path="/" component={ Login } />
+        <Route path="/carteira" component={ Wallet } />
+      </main>
+    );
+  }
 }
 
 export default App;
