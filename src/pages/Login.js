e71135c0369858ba9630e3bb17de1c340b8358ed
jefersongjr <jeferson_gjr@hotmail.com>
2022-06-23 19:05:24 -0300

    criando estado de user

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,35 @@
 import React from 'react';
 
 class Login extends React.Component {
-  render() {
-    return (
-      <section className="login-box">
-        Username:
-        <input name="login-name" type="email" data-testid="email-input" />
-        Password:
-        <input name="password-name" type="password" data-testid="password-input" />
-        <button type="button">
-          Entrar
-        </button>
-      </section>
-    );
-  }
+ state = {
+   email: '',
+ }
+
+ handleChange = ({ target }) => {
+   const { name, value } = target;
+   this.setState({ [name]: value });
+ }
+
+ render() {
+   const { email } = this.state;
+   return (
+     <section className="login-box">
+       Username:
+       <input
+         name="email"
+         value={ email }
+         type="email"
+         onChange={ this.handleChange }
+         data-testid="email-input"
+       />
+       Password:
+       <input name="password-name" type="password" data-testid="password-input" />
+       <button type="button">
+         Entrar
+       </button>
+     </section>
+   );
+ }
 }
 
 export default Login;
