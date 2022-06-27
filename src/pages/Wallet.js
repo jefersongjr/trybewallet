21931f9b2146fba3a58b3ee32774b2493ac36da9
jefersongjr <jeferson_gjr@hotmail.com>
2022-06-26 21:31:56 -0300

    Excluindo Chave usdt

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,8 +2,13 @@ import React from 'react';
 import Forms from '../components/Forms';
 import Header from '../components/Header';
 import './Wallet.css';
+import getEconomyApi from '../services/econonyApiFetch';
 
 class Wallet extends React.Component {
+  componentDidMount() {
+    getEconomyApi();
+  }
+
   render() {
     return (
       <div>
--- src/services/econonyApiFetch.js
+++ src/services/econonyApiFetch.js
@@ -3,7 +3,9 @@ const ENDPOINT = 'https://economia.awesomeapi.com.br/json/all';
 const getEconomyApi = async () => {
   const response = await fetch(ENDPOINT);
   const json = await response.json();
-  return json;
+  const jsonArray = Object.entries(json);
+  const filter = jsonArray.splice(1, 1);
+  return filter;
 };
 
 export default getEconomyApi;
